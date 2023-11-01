6a1655ff59b6f9d6bbe5afb6eff5b26f1f86b5f1
waterboy96 <sebastian.topalian@gmail.com>
2023-11-01 12:55:50 +0100

    Updated markdown

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import find_packages, setup
 
-
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 
 setup(
     name="mvSPC",
-    version="1.0.12",
+    version="1.0.13",
     author="Henrik Hviid Hansen, Sebastian Olivier Nymann Topalian, Davide Cacciarelli",
     author_email="hehha@orsted.com, sebtop@kt.dtu.dk, dcac@dtu.dk",
     description="Plug and play statistical process control functions for Python",
-    long_description="Python package based on work from 3 PhD projects at the Technical University of Denmark covering multivariate statistical process control (MSPC), autoencoders for MSPC and determination of delays for dynamic principal component analysis",
+    long_description=long_description,
     long_description_content_type="markdown",
     url="https://github.com/hviidhenrik/pySPC",
     packages=find_packages(exclude=["tests*"]),
